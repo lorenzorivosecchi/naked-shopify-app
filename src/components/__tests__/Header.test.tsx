c658709e1060abd9650ae58f024540307ef85ce3
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2021-03-02 17:53:23 +0100

    test: header displays shop name

--- src/components/__tests__/Header.test.tsx
+++ src/components/__tests__/Header.test.tsx
@@ -1,21 +1,30 @@
 import Header from "../Header";
-import { render, waitFor } from "@testing-library/react";
+import { findByText, render, waitFor } from "@testing-library/react";
 import AutoMockedProvider from "../../utils/testing/AutoMockedProvider";
 
 describe("Header", () => {
   it("is truthy", () => {
     expect(Header).toBeTruthy();
   });
-  it("displays a spinner", async () => {
+  it("displays the shop name", async () => {
+    const mocks = {
+      Shop: {
+        name: () => "My Shop",
+      },
+    };
+
     const { getByText } = render(
-      <AutoMockedProvider>
+      <AutoMockedProvider mocks={mocks}>
         <Header />
       </AutoMockedProvider>
     );
 
+    const loading = getByText(/Loading.../i);
+    expect(loading).toBeInTheDocument();
+
     await waitFor(() => {
-      const loading = getByText(/Loading.../i);
-      expect(loading).toBeInTheDocument();
+      const shopName = getByText(/My Shop/i);
+      expect(shopName).toBeInTheDocument();
     });
   });
 });
