c7ebfc1251c5480ac9e374395f521667ab0fc6a9
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2021-03-02 17:49:49 +0100

    chore: add ts-jest

--- src/components/__tests__/Header.test.tsx
+++ src/components/__tests__/Header.test.tsx
@@ -1,7 +1,21 @@
 import Header from "../Header";
+import { render, waitFor } from "@testing-library/react";
+import AutoMockedProvider from "../../utils/testing/AutoMockedProvider";
 
 describe("Header", () => {
   it("is truthy", () => {
     expect(Header).toBeTruthy();
+  });
+  it("displays a spinner", async () => {
+    const { getByText } = render(
+      <AutoMockedProvider>
+        <Header />
+      </AutoMockedProvider>
+    );
+
+    await waitFor(() => {
+      const loading = getByText(/Loading.../i);
+      expect(loading).toBeInTheDocument();
+    });
   });
 });
