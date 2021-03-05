564bd6b0e9fba9ff6ad4a507b3977347250238d7
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2021-03-05 22:02:42 +0100

    feat: add title and links on auth pages

--- src/pages/account/login.tsx
+++ src/pages/account/login.tsx
@@ -1,8 +1,17 @@
 import { NextPage } from "next";
 import LoginForm from "src/components/LoginForm";
+import Link from "next/link";
 
 const Login: NextPage<{}> = () => {
-  return <LoginForm onSubmit={() => alert("Welcome")} />;
+  return (
+    <>
+      <h1>Login</h1>
+      <LoginForm onSubmit={() => alert("Welcome")} />
+      <Link href="/account/register">
+        <a>I don't have an account</a>
+      </Link>
+    </>
+  );
 };
 
 export default Login;
--- src/pages/account/register.tsx
+++ src/pages/account/register.tsx
@@ -1,10 +1,15 @@
 import { NextPage } from "next";
 import RegisterForm from "src/components/RegisterForm";
+import Link from "next/link";
 
 const Register: NextPage<{}> = () => {
   return (
     <>
+      <h1>Register</h1>
       <RegisterForm onSubmit={() => alert("Welcome")} />
+      <Link href="/account/login">
+        <a>I already have an account</a>
+      </Link>
     </>
   );
 };
