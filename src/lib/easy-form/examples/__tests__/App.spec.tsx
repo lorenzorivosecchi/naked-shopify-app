75ef03c3c4615a977e41fdfd019f22d19d56c1df
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2021-03-05 13:04:26 +0100

    test: verify that form values can be updated

--- src/lib/easy-form/examples/__tests__/App.spec.tsx
+++ src/lib/easy-form/examples/__tests__/App.spec.tsx
@@ -1,18 +1,35 @@
-import { getByLabelText, render } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "../App";
 
 describe("App", () => {
-  it("renders a form", () => {
+  it("works", async () => {
     const { getByLabelText, getByRole } = render(<App />);
 
     const form = getByLabelText(/profile/i);
     expect(form).toBeInTheDocument();
 
-    const firstName = getByLabelText(/first name/i);
-    expect(firstName).toBeInTheDocument();
+    expect(form).toHaveFormValues({
+      firstName: "",
+      lastName: "",
+      gender: "Male",
+    });
 
+    const firstName = getByLabelText(/first name/i);
     const lastName = getByLabelText(/last name/i);
-    expect(lastName).toBeInTheDocument();
+    const gender = getByLabelText(/gender/i);
+
+    userEvent.type(firstName, "Lisa");
+    userEvent.type(lastName, "Simpson");
+    userEvent.selectOptions(gender, "Female");
+
+    await waitFor(() => {
+      expect(form).toHaveFormValues({
+        firstName: "Lisa",
+        lastName: "Simpson",
+        gender: "Female",
+      });
+    });
 
     const submit = getByRole("button", { name: /update/i });
     expect(submit).toBeInTheDocument();
