4e8ed8652f591da9ef7f59bedf5746745adfadc9
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2021-03-06 21:33:33 +0100

    feat: verify if old token is valid before running customer query

--- src/components/AuthProvider.tsx
+++ src/components/AuthProvider.tsx
@@ -1,5 +1,5 @@
-import { gql, useLazyQuery, useMutation } from "@apollo/client";
-import { ReactNode } from "react";
+import { gql, useMutation, useQuery } from "@apollo/client";
+import { ReactNode, useEffect, useMemo, useState } from "react";
 import { AuthContext } from "src/utils/context/auth";
 import { CustomerName } from "./__generated__/CustomerName";
 import { Login, Login_customerAccessTokenCreate } from "./__generated__/Login";
@@ -64,28 +64,61 @@ const CUSTOMER_NAME = gql`
   }
 `;
 
-/** Stores customer access token and expiration date into localStorage */
-const onAccessTokenCreated = (
-  result: Login_customerAccessTokenCreate | Register_customerAccessTokenCreate
-) => {
-  const { accessToken, expiresAt } = result.customerAccessToken;
-  localStorage.setItem("CUSTOMER_TOKEN", accessToken);
-  localStorage.setItem("CUSTOMER_TOKEN_EXPIRES_AT", expiresAt);
-};
-
-/** Removes customer access token and expiration date from localStorage */
-const onAccessTokenDeleted = () => {
-  localStorage.removeItem("CUSTOMER_TOKEN");
-  localStorage.removeItem("CUSTOMER_TOKEN_EXPIRES_AT");
-};
-
 interface Props {
   children: ReactNode;
 }
 
 const AuthProvider: React.FC<Props> = ({ children }) => {
-  const [_, { data }] = useLazyQuery<CustomerName>(CUSTOMER_NAME);
-  const customer = data?.customer || null;
+  // Keep track of old token and its expiration date.
+  const [oldToken, setOldToken] = useState<string>();
+  const [oldTokenHasExpired, setOldTokenHasExpired] = useState<boolean>();
+
+  // Checks inside localStorage for token and expiration date.
+  useEffect(() => {
+    const token = localStorage.getItem("CUSTOMER_TOKEN");
+    const tokenExpiresAt = localStorage.getItem("CUSTOMER_TOKEN_EXPIRES_AT");
+
+    // Set token or null in case there isn't one.
+    setOldToken(token);
+
+    if (tokenExpiresAt) {
+      const hasExpired = new Date() > new Date(tokenExpiresAt);
+      setOldTokenHasExpired(hasExpired);
+    }
+  }, []);
+
+  // - Runs customer query if old credentials are valid.
+  // - Exposes function to refetch data after a mutation.
+  const { data: customerQueryData, refetch } = useQuery<CustomerName>(
+    CUSTOMER_NAME,
+    {
+      // Skip initial query in case token doesn't exist or it has expired.
+      skip: !oldToken || oldTokenHasExpired,
+      variables: {
+        token: oldToken,
+      },
+    }
+  );
+
+  /** Stores customer access token and expiration date into localStorage */
+  const onAccessTokenCreated = (
+    result: Login_customerAccessTokenCreate | Register_customerAccessTokenCreate
+  ) => {
+    const { accessToken, expiresAt } = result.customerAccessToken;
+    localStorage.setItem("CUSTOMER_TOKEN", accessToken);
+    localStorage.setItem("CUSTOMER_TOKEN_EXPIRES_AT", expiresAt);
+    // Refetch customer query
+    refetch({
+      token: accessToken,
+    });
+  };
+
+  /** Removes customer access token and expiration date from localStorage */
+  const onAccessTokenDeleted = () => {
+    localStorage.removeItem("CUSTOMER_TOKEN");
+    localStorage.removeItem("CUSTOMER_TOKEN_EXPIRES_AT");
+    // TODO: invalidate cached response for customer query
+  };
 
   const [login] = useMutation<Login>(LOGIN, {
     onCompleted(data) {
@@ -104,6 +137,8 @@ const AuthProvider: React.FC<Props> = ({ children }) => {
       onAccessTokenDeleted();
     },
   });
+
+  const customer = customerQueryData?.customer || null;
 
   return (
     <AuthContext.Provider value={{ login, logout, register, customer }}>
