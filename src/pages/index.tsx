5d9d25dc3dcff5fcf72e4499452ef1b1fce38166
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2021-03-07 15:04:14 +0100

    feat: show welcome message

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,3 @@
-import { useEffect } from "react";
-import { useRouter } from "next/router";
-
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push("/account/login");
-  }, []);
-
-  return null;
+  return <h1>Welcome</h1>;
 }
