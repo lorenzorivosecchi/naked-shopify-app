70bffdbbfdc37a1f521ea327579160c0dc901ac2
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2021-03-13 21:34:37 +0100

    chore: add comment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   env: {
     shopifyStorefrontApiURL: process.env.SHOPIFY_STOREFRONT_API_URL,
+    // This token will be available to the browser (it's ok)
     shopifyStorefrontApiToken: process.env.SHOPIFY_STOREFRONT_API_TOKEN,
   },
 };
